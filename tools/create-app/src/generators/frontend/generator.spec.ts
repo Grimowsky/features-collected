59be339ea53e0b6a7a422540a5110557c9884d4b
Grimm <rafalmirkowski@gmail.com>
2024-09-26 10:51:16 +0200

    add generators for backend and frontend

---
+++ tools/create-app/src/generators/frontend/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nx/devkit';
+
+import { frontendGenerator } from './generator';
+import { FrontendGeneratorSchema } from './schema';
+
+describe('frontend generator', () => {
+  let tree: Tree;
+  const options: FrontendGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await frontendGenerator(tree, options);
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config).toBeDefined();
+  });
+});
